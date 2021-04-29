89cee977272a5cd58ff2bb665fbc2af9c015b95c
hushui <1114524585@qq.com>
2021-04-29 15:05:15 +0800

    Spring Boot 整合 Mybatis
    	新文件：   demo28/.idea/.gitignore
    	新文件：   demo28/.idea/compiler.xml
    	新文件：   demo28/.idea/jarRepositories.xml
    	新文件：   demo28/.idea/misc.xml
    	新文件：   demo28/.idea/vcs.xml
    	新文件：   demo28/demo28.iml
    	新文件：   demo28/pom.xml
    	新文件：   demo28/src/main/java/com/hushuli/Application.java
    	新文件：   demo28/src/main/java/com/hushuli/controller/UserHander.java
    	新文件：   demo28/src/main/java/com/hushuli/entity/User.java
    	新文件：   demo28/src/main/java/com/hushuli/repository/UserRepository.java
    	新文件：   demo28/src/main/resources/application.yml
    	新文件：   demo28/src/main/resources/mapping/userMapper.xml
    	新文件：   demo28/target/classes/application.yml
    	新文件：   demo28/target/classes/com/hushuli/Application.class
    	新文件：   demo28/target/classes/com/hushuli/controller/UserHander.class
    	新文件：   demo28/target/classes/com/hushuli/entity/User.class
    	新文件：   demo28/target/classes/com/hushuli/repository/UserRepository.class
    	新文件：   demo28/target/classes/mapping/userMapper.xml

---
+++ demo28/src/main/java/com/hushuli/controller/UserHander.java
@@ -0,0 +1,40 @@
+package com.hushuli.controller;
+
+import com.hushuli.entity.User;
+import com.hushuli.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
+public class UserHander {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("findAll")
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("findById/{id}")
+    public User findById(@PathVariable("id") long id) {
+        return userRepository.findById(id);
+    }
+
+    @PostMapping("save")
+    public void save(@RequestBody User user) {
+        userRepository.save(user);
+    }
+
+    @PutMapping("update")
+    public void update(@RequestBody User user) {
+        userRepository.update(user);
+    }
+
+    @DeleteMapping("deleteById/{id}")
+    public void deleteById(@PathVariable("id") long id) {
+        userRepository.deleteById(id);
+    }
+}
